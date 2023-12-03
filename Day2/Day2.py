c1b81a7062e71194ee03208b62ae953745fbf6a3
trmach <charlessaineghi@gmail.com>
2023-12-03 17:50:12 -0600

    Day 2 completed :snail:

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -3,8 +3,8 @@ import sys
 def solve1(input):
     colormap = {"red":0, "green":1, "blue":2}
     games = 0
-    for s in range(len(input)):
-        game = input[s].split(':')
+    for s in input:
+        game = s.split(':')
         gamenumber = int(game[0].split()[1])
         hands = game[1].split(';')
         possible = True
@@ -23,6 +23,18 @@ def solve1(input):
 
 def solve2(input):
     colormap = {"red":0, "green":1, "blue":2}
+    sum = 0
+    for s in input:
+        game = s.split(':')
+        hands = game[1].split(';')
+        minima = [0, 0, 0]
+        for hand in hands:
+            for cubes in hand.split(','):
+                tokens = cubes[1:].split()
+                minima[colormap[tokens[1]]] = max(minima[colormap[tokens[1]]], int(tokens[0]))
+        sum += (lambda x, y, z: x*y*z)(minima[0], minima[1], minima[2])
+
+    return sum
 
 
 def main():
@@ -33,6 +45,7 @@ def main():
         with open(sys.argv[1], 'r') as file:
             input = [x[:-1] for x in file.readlines()] # much cleaner
             print(solve1(input))
+            print(solve2(input))
 
 if __name__ == '__main__':
     main()
