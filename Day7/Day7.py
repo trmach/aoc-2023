5dcd1b4043bbf0dd9cf51262eb891a14fb227470
trmach <charlessaineghi@gmail.com>
2023-12-09 11:37:37 -0600

    Day 7 part 2 :black_joker:

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -9,6 +9,7 @@ import sys
 # if length 5 => High card => set(1)
 
 map_ = {'A':14, 'K':13, 'Q':12, 'J':11, 'T':10, '9':9, '8':8, '7':7, '6':6, '5':5, '4':4, '3':3, '2':2}
+mapJ = {'A':14, 'K':13, 'Q':12, 'J':1, 'T':10, '9':9, '8':8, '7':7, '6':6, '5':5, '4':4, '3':3, '2':2}
 
 class Hand:
     def __init__(self, hand=''):
@@ -58,27 +59,47 @@ class Hand:
                     return map_[x] < map_[y]
     
         return False
-
-    # def __eq__(self, other): # x.__eq__(y) <=> x == y; x has a rank equal to y? do we need this?
-    #     pass
-
-    # def __gt__(self, other): # x.__gt__(y) <=> x > y; x has a rank greater than y
-    #     if self.classify() > other.classify():
-    #         return True
-    #     elif self.classify() < other.classify():
-    #         return False
-    #     else:
-    #         for x, y in zip(self.hand, other.hand):
-    #             if map_[x] == map_[y]:
-    #                 continue
-    #             else:
-    #                 return map_[x] > map_[y]
-    
-    #     return False
     
     def __repr__(self):
         return repr(self.hand)
+
+class HandJ:
+    def __init__(self, hand=''):
+        self.hand = hand
+
+    def classify(self):
+        if 'J' not in self.hand:
+            return Hand(self.hand).classify()
+        else:
+            dself = {}
+            for x in self.hand:
+                if x != 'J':
+                    if x not in dself: dself[x] = 1
+                    else: dself[x] += 1
+            max = ('0', 0)
+            for k, v in dself.items(): # get most frequently occuring 
+                if v > max[1]:
+                    max = (k, v)
+            return Hand(self.hand.replace('J', max[0])).classify()
+            
+    def __lt__(self, other): # x.__lt__(y) <=> x < y; x has a lower rank than y
+        
+        if self.classify() < other.classify():
+            return True
+        elif self.classify() > other.classify():
+            return False
+        else:
+            for x, y in zip(self.hand, other.hand):
+                if mapJ[x] == mapJ[y]:
+                    continue
+                else:
+                    return mapJ[x] < mapJ[y]
     
+        return False
+    
+    def __repr__(self):
+        return repr(self.hand)
+
 # ***This is a Min Heap*** #
 class Heap: # thanks to Prof. Lee for teaching me DS and providing valuable resources
     def __init__(self):
@@ -158,7 +179,20 @@ def solve1(input):
     return total
 
 def solve2(input):
-    pass
+    h = Heap()
+    hands = [(HandJ(x[0]), int(x[1])) for x in [y.split() for y in input]]
+
+    for hand in hands:
+        h.add(hand)
+    
+    l = []
+    while h:
+        l.append(h.pop())
+    total = 0
+    for x, y in enumerate(l):
+        total += (x+1)*y[1]
+
+    return total
 
 def main():
     if len(sys.argv) < 2:
